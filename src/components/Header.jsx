a2771455f5f6b556f6c4c4134cfd8f76382f6200
CarlaMSLopes <carlalopes613@outlook.com>
2025-03-25 22:49:16 -0300

    :tada: first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import styles from './Header.module.css';
+import logo from '../assets/logo-ignite.png';
+
+export function Header(){
+    return (
+        <header className={styles.header}>
+            <img 
+                src={logo} 
+                alt="Logo ab filmes" 
+            />
+            <strong>Ignite Feed</strong>
+        </header>
+    )
+}
