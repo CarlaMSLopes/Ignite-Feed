de39019d21fe77d08055e5df3b3c97c002b8f3bc
CarlaMSLopes <carlalopes613@outlook.com>
2025-03-28 14:54:15 -0300

    :lipstick: feat: componente comentario

--- src/components/Comentario/Comentario.jsx
+++ src/components/Comentario/Comentario.jsx
@@ -4,29 +4,28 @@ import styles from "./Comentario.module.css";
 
 export function Comentario() {
   return (
-    <>
-      <main className={styles.comentario}>
-        <Avatar src="https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=1522&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        <div className={styles.boxContent}>
-          <header>
-            <div className={styles.namePerfil}>
-              <strong>Jenny Le Blanc</strong>
-              <span>Cerca de 1 hora</span>
+    <main className={styles.comentario}>
+      <Avatar src="https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=1522&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <div className={styles.comentarioSide}>
+        <div className={styles.bgComentario}>
+          <header className={styles.headerComentario}>
+            <div>
+              <strong>Devon Lane</strong>
+              <span>Cerca de 2h</span>
             </div>
-            <button><Trash className={styles.iconTrash} /></button>            
+            <button>
+              <Trash className={styles.iconTrash} />
+            </button>
           </header>
-          <div>
-            <p>Muito bom Devon, parabéns!! 👏👏</p>
-          </div>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
         </div>
-      </main>
-      <footer>
-        <button>
-          <ThumbsUp className={styles.like} />
-          Aplaudir
-        </button>
-        <p>• 24</p>
-      </footer>
-    </>
+        <div>
+          <button className={styles.iconLike}><ThumbsUp  />Aplaudir • 34</button>
+        </div>
+      </div>
+    </main>
   );
+}
+{
+  /* <Avatar src="https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=1522&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" /> */
 }
